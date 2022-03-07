3693631cd07d96d94fc79443ddb4ee4b9be08f49
Sakhavat <sakhavatra@code.edu.az>
2022-03-07 15:07:05 +0400

    nav-mobil-ready

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@ CatBtn.addEventListener("click",function(){
     Cat_items.classList.toggle("show")
     catIconBtn.classList.toggle("CatIconRotate")
 })
+
+$(function() {
+
+    $('.sidebar-menu i').click(function(){
+        $(this).parents('.dropdown').find('.dropdown').not($(this).siblings('ul')).slideUp();
+        $(this).siblings('ul').stop(true,true).slideToggle();
+        $(this).toggleClass('active');
+    });
+
+    $('#btn-open-navbar, #btn-close-navbar, .overlay').click(function(){
+        $('.overlay, .sidebar').toggleClass('active');
+    })
+});
