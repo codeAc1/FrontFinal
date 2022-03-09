48fae9484396d5e1a62a055d36f8d1e93de63fec
Sakhavat <sakhavatra@code.edu.az>
2022-03-09 13:58:27 +0400

    contact-dont ready

--- js/main.js
+++ js/main.js
@@ -1,21 +1,7 @@
-///Catagories Btn Aktiv
-let CatBtn=document.querySelector("#catBtn");
-let Cat_items=document.querySelector("#cat_items_btn")
-let catIconBtn=document.querySelector("#catIconBtn");
-CatBtn.addEventListener("click",function(){
-    Cat_items.classList.toggle("show")
-    catIconBtn.classList.toggle("CatIconRotate")
-})
 
-$(function() {
+ AOS.init();
 
-    $('.sidebar-menu i').click(function(){
-        $(this).parents('.dropdown').find('.dropdown').not($(this).siblings('ul')).slideUp();
-        $(this).siblings('ul').stop(true,true).slideToggle();
-        $(this).toggleClass('active');
-    });
 
-    $('#btn-open-navbar, #btn-close-navbar, .overlay').click(function(){
-        $('.overlay, .sidebar').toggleClass('active');
-    })
-});
+
+
+
