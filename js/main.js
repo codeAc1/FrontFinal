475a74bd5744d280fdc3d2748ae12ad15f225ca9
Sakhavat <sakhavatra@code.edu.az>
2022-03-10 18:44:24 +0400

    ok

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 
  AOS.init();
 
+let ProductList =document.querySelectorAll("#ProList .product-cart .pro-bottom .add-cart")
+for(AddCartBtn of ProductList){
+    AddCartBtn.addEventListener("click",function(e){
+        e.preventDefault()
+        alert("ok")
+    })
+}
+
+
 
  
 
