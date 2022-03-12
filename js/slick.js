699aff9dc0cafcf7f5b6bd0ce864795a93c96558
Sakhavat <sakhavatra@code.edu.az>
2022-03-13 03:53:51 +0400

    Ready

--- js/slick.js
+++ js/slick.js
@@ -1,8 +1,8 @@
 $('.slick').slick({
     lazyLoad: 'ondemand',
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     infinite: true,
-    autoplay: true,
+    autoplay: false,
     arrows: true,
   });
